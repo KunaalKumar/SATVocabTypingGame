292aaf233866b1f02e80788294299dcc5763227d
studydude <studydude@protonmail.com>
2018-11-26 14:19:58 -0700

    manage resources file && change word.txt position

---
+++ SpaceTurtleSpeedTyping/build-GameWindow-Desktop-Debug/ui_gamewindow.h
@@ -0,0 +1,66 @@
+/********************************************************************************
+** Form generated from reading UI file 'gamewindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_GAMEWINDOW_H
+#define UI_GAMEWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_GameWindow
+{
+public:
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QWidget *centralWidget;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *GameWindow)
+    {
+        if (GameWindow->objectName().isEmpty())
+            GameWindow->setObjectName(QStringLiteral("GameWindow"));
+        GameWindow->resize(400, 300);
+        menuBar = new QMenuBar(GameWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        GameWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(GameWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        GameWindow->addToolBar(mainToolBar);
+        centralWidget = new QWidget(GameWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        GameWindow->setCentralWidget(centralWidget);
+        statusBar = new QStatusBar(GameWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        GameWindow->setStatusBar(statusBar);
+
+        retranslateUi(GameWindow);
+
+        QMetaObject::connectSlotsByName(GameWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *GameWindow)
+    {
+        GameWindow->setWindowTitle(QApplication::translate("GameWindow", "GameWindow", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class GameWindow: public Ui_GameWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_GAMEWINDOW_H
