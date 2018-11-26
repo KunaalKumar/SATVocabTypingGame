bc6745001cb5944dcc3739f0c6edadb32a23afdd
Soren Nelson <sorennelson33@gmail.com>
2018-11-25 19:10:58 -0700

    Sort word file

--- SpaceTurtleSpeedTyping/GameLib/gamelib.cpp
+++ SpaceTurtleSpeedTyping/GameLib/gamelib.cpp
@@ -4,3 +4,41 @@
 GameLib::GameLib()
 {
 }
+
+//void sortWordTxtFile()
+//{
+//    std::fstream stream;
+//     stream.open("../GameLib/temp");
+//     std::vector<std::string> words;
+//     if (stream.is_open())
+//     {
+//        std::string line;
+//        int i = 0;
+//        while (!stream.eof())
+//        {
+//            std::getline(stream, line);
+//            if (line.length() > 0)
+//            {
+//                words.push_back(line);
+//            }
+//         }
+//    }
+
+//     std::sort(words.begin(), words.end(),
+//           [](const std::string& lhs, const std::string& rhs)
+//            {
+//                return lhs.size() == rhs.size() ? lhs < rhs : lhs.size() < rhs.size();
+//            });
+
+//     words.erase( unique( words.begin(), words.end() ), words.end() );
+
+//     stream.close();
+
+//     stream.open("../GameLib/words");
+//     for (int i = 0; i < words.size(); i++)
+//     {
+//        stream << words.at(i) << std::endl;
+//     }
+
+//    stream.close();
+//}
