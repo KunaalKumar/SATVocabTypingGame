7116fa2b56a4514a1006c5e8939f8e05b10b1803
Soren Nelson <sorennelson33@gmail.com>
2018-11-27 00:03:17 -0700

    Implement shoot enemy for backend and remove QTimer for enemy creation  from backend

--- SpaceTurtleSpeedTyping/GameLib/gamelib.cpp
+++ SpaceTurtleSpeedTyping/GameLib/gamelib.cpp
@@ -9,11 +9,14 @@ GameLib::GameLib()
     totalKillCount = 0;
 }
 
+GameLib::~GameLib()
+{
+    delete currentEnemy;
+}
+
 void GameLib::startRound()
 {
-    round++;
-    Load::createRoundWords(round);
-    QTimer::singleShot(0, this, SLOT(createEnemies()));
+    Load::createRoundWords(++round);
 }
 
 void GameLib::createEnemies()
@@ -22,12 +25,41 @@ void GameLib::createEnemies()
     if (enemy.getWord() != "")
     {
         currentEnemies.push_back(Enemy(round));
-
-        int enemyInterval = 2500;
-        QTimer::singleShot(enemyInterval, this, SLOT(createEnemies()));
     }
 }
 
+bool GameLib::shoot(QString letter)
+{
+    if (currentEnemy == nullptr)
+    {
+        // shoot closest enemy with starting letter = letter
+        //TO FIX: put in real user position
+        QPoint userPos(600, 300);
+        double lowestDistance = DBL_MAX;
+        for (Enemy enemy : currentEnemies)
+        {
+            if (enemy.startsWith(letter.toStdString()))
+            {
+                double distance = enemy.distanceTo(userPos);
+
+                if (distance < lowestDistance)
+                {
+                    lowestDistance = distance;
+                    currentEnemy = &enemy;
+                }
+            }
+        }
+        if (currentEnemy != nullptr)
+        {
+            currentEnemy->shoot(letter.toStdString());
+        }
+        return currentEnemy == nullptr;
+    }
+    else
+    {
+        return currentEnemy->shoot(letter.toStdString());
+    }
+}
 
 
 
