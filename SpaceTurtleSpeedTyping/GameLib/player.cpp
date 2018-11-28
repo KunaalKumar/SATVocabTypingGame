83e97ef876b354cab62dfb13b451f1c414fc4456
Nathan <nathanboehm956@gmail.com>
2018-11-27 19:02:20 -0700

    added type inits for projectile and player.

--- SpaceTurtleSpeedTyping/GameLib/player.cpp
+++ SpaceTurtleSpeedTyping/GameLib/player.cpp
@@ -5,6 +5,7 @@ namespace DrawableObjects {
 Player::Player() : DrawableObject()
 {
     this->health = 3;
+    this->type = Type::player;
 }
 
 Player::Player(unsigned int health, posTuple pos) : DrawableObject(pos)
