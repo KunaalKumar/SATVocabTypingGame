330f8ad5dbe78933e070755309efba6aaf9fb17c
victoriayue <victoriayue05@gmail.com>
2018-12-06 14:33:25 -0700

    modify multiple words

--- SpaceTurtleSpeedTyping/GameWindow/gameview.h
+++ SpaceTurtleSpeedTyping/GameWindow/gameview.h
@@ -6,6 +6,7 @@
 #include <QMediaPlayer>
 #include "gamelib.h"
 #include "gameobjects.h"
+#include "QVector"
 
 namespace Ui {
 class GameView;
@@ -29,6 +30,7 @@ signals:
     void homeClicked();
 
 private slots:
+    void addWord();
 
 
 private:
@@ -42,9 +44,13 @@ private:
     sf::Sprite sprite;
     sf::Font font;
     sf::Text text;
+    sf::Text texts[10];
+    int text_idx;
+
     QMediaPlayer fireSound;
     QVector<std::string> textVector;
     QTimer *timer;
+    QTimer *timer2;
     int count, firedms; // firedms determines the hit strike animation duration
     bool fired;
     float pXpos;
@@ -58,6 +64,7 @@ private:
     void initGameObjects(std::vector<GameObjects::GameObject *>);
     void fire(float x1, float y1, float x2, float y2);
     void endGame();
+    void updateLives(int lives);
 
 };
 
