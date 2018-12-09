42029a2f38187532a0b1ef44cd09d25dc17ea8ef
Soren Nelson <sorennelson33@gmail.com>
2018-12-09 01:05:59 -0700

    Fix tutorial

--- SpaceTurtleSpeedTyping/GameWindow/gamewindow.cpp
+++ SpaceTurtleSpeedTyping/GameWindow/gamewindow.cpp
@@ -37,9 +37,17 @@ void GameWindow::on_gameStartButton_clicked()
 
 void GameWindow::on_tutorialButton_clicked()
 {
-    QString tutorial = "Press START to start the game. \n"
-                       "Hit the enermy by spelling the word using keyboard. \n"
-                       "There are three lives total. If user miss one enermy, they will lose one lives. \n";
+    QString tutorial = "Press START EASY GAME to start the game with the words advancing in length as you go. \n"
+                        "\n"
+                       "Press START SAT PREP to start the game with the words being the SAT Prep words advancing in words per round. \n"
+                        "\n"
+                       "Hit the enemy by spelling the word using keyboard. \n"
+                        "\n"
+                       "Once you target an enemy you must kill it. If you misspell the word you have to start the word over. \n"
+                        "\n"
+                       "If you get hit by an enemy you lose a life. You have three lives total. \n"
+                        "\n"
+                        "Good Luck.";
     QMessageBox::StandardButton box = QMessageBox::information(this, "Tutorial", tutorial, QMessageBox::Ok, QMessageBox::NoButton);
 }
 
